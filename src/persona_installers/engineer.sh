f1c054b573814888e06fffe0de97017f58e12e63
mvim <1715206+MatthewVi@users.noreply.github.com>
2018-09-11 23:34:56 -0400

    compose

--- src/persona_installers/engineer.sh
+++ src/persona_installers/engineer.sh
@@ -331,6 +331,8 @@ sudo snap install slack-term
 sudo snap install docker
 sudo groupadd docker
 sudo usermod -aG docker $USER
+sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 sudo npm install yo --global
 
