95709d58419ae055a10f3c31a8fcc4ca006e44a3
Matthew Vi <matthewvita48@gmail.com>
2018-09-15 11:22:52 -0400

    Basic clinician persona

--- personas/clinician/clinician.sh
+++ personas/clinician/clinician.sh
@@ -1,30 +1,38 @@
 #!/bin/bash
 # System 💖
 
+# TODOs:
+#   - Local OpenEMR with NAMCS dataset
+#   - Make menu links for each package
+#   - More software and curated resources
+
 cd /home/`whoami`/Desktop/
 
-############
-### TODO ###
-############
+make_clinical_url() {
+eval name="$1"
+eval url="$2"
 
-### https://github.com/kakoni/awesome-healthcare !!!
+touch ${name}.desktop
 
-# More namcs sources
-# Openemr cloud link
-# dch4chee
-# folding at home
-# git clone https://github.com/smart-on-fhir/fhir-viewer.git
-# github public data healthcare sets
-# healthcare fake data generator from Nurse Robert or a rec.
-# https://github.com/MIT-LCP/mimic-code
-# https://github.com/beamandrew/medical-data
-# https://github.com/braytonstafford/farser
-# https://github.com/jamesagnew/hapi-fhir
-# https://github.com/johnschrom/ambulatory-ehr-data
-# https://github.com/kakoni/awesome-healthcare
-# https://github.com/smart-on-fhir/sample-patients
-# ctakes link
-# https://simon.kde.org/
-# link to open health hub
+echo -e "
+[Desktop Entry]
+Encoding=UTF-8
+Name=${name}
+Type=Link
+URL=${url}
+Icon=text-html
+" > ${name}.desktop
+}
 
-# TODO: essential offline medical resources
+make_clinical_url "OpenEMR Install" "https://aws.amazon.com/marketplace/pp/B07BBT4C1H/" 
+
+sudo apt install -y libosmesa6-dev libglu1-mesa-dev libfontconfig-dev libxrender-dev libncurses5-dev
+wget http://slicer.kitware.com/midas3/download/bitstream/738960/Slicer-4.8.1-linux-amd64.tar.gz
+tar xvf Slicer-4.8.1-linux-amd64.tar.gz
+rm Slicer-4.8.1-linux-amd64.tar.gz
+mv Slicer-4.8.1-linux-amd64/ Slicer-Dicom-Viewer
+
+sudo npm install nativefier -g
+
+nativefier --name "NIH Pubmed" "https://www.ncbi.nlm.nih.gov/pubmed/"
+nativefier --name "Medscape Reference" "https://reference.medscape.com/"
