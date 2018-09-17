210d930e90d9c9f38ef9263f3a121c756b39390e
mvim <1715206+MatthewVi@users.noreply.github.com>
2018-09-16 22:50:02 -0400

    Minor cleanup to gamer persona

--- personas/gamer/gamer.sh
+++ personas/gamer/gamer.sh
@@ -3,10 +3,6 @@
 
 cd /home/`whoami`/Desktop/
 
-sudo wget http://deb.playonlinux.com/playonlinux_precise.list -O /etc/apt/sources.list.d/playonlinux.list
-sudo apt update -y
-sudo apt install -y playonlinux
-
 sudo apt install -y yabause
 
 sudo add-apt-repository -y ppa:wfg/0ad
@@ -25,11 +21,6 @@ sudo apt install -y pcsx2
 
 sudo apt install -y torcs
 
-# TODO: Consistently slow d/l. Look for another ppa.
-# sudo add-apt-repository -y ppa:saiarcot895/flightgear
-# sudo apt update -y
-# sudo apt install -y flightgear
-
 sudo apt install -y freeciv
 
 sudo apt install -y freedoom
@@ -41,10 +32,6 @@ sudo apt install -y slashem
 echo "deb http://download.opensuse.org/repositories/home:/FrodeSolheim:/stable/Debian_9.0/ /" > /etc/apt/sources.list.d/FrodeSolheim-stable.list
 sudo apt-get update -y
 sudo apt-get install -y fs-uae fs-uae-launcher fs-uae-arcade
-
-wget https://github.com/Stellarium/stellarium/releases/download/v0.18.2/Stellarium-0.18.2-x86_64.AppImage
-
-wget https://github.com/OpenRCT2/OpenRCT2/releases/download/v0.2.1/OpenRCT2-0.2.1-linux-x86_64.tar.gz
 
 sudo apt install -y minetest
 
@@ -80,10 +67,10 @@ sudo add-apt-repository -y ppa:mumble/release
 sudo apt update -y
 sudo apt install -y mumble
 
-sudo add-apt-repository -y ppa:openmw/openmw
-sudo apt update -y
-sudo apt install -y openmw-cs openmw openmw-launcher
-
 sudo snap install gnome-twitch --beta
+
+sudo wget http://deb.playonlinux.com/playonlinux_precise.list -O /etc/apt/sources.list.d/playonlinux.list
+sudo apt update -y
+sudo apt install -y playonlinux
 
 git clone https://github.com/leereilly/games.git
